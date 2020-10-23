d108fa4acaf5d3ec903c978ea1583172e70a86a0
Arthur Henrique <arthur393@gmail.com>
2020-10-23 16:15:15 -0300

    refactor

--- main.go
+++ main.go
@@ -9,14 +9,11 @@ import (
 func main() {
 	client := gosseract.NewClient()
 	defer client.Close()
-	// client.SetConfigFile("/usr/local/Cellar/tesseract/4.1.1/share/tessdata/tessconfigs/test")
-	// client.SetVariable(gosseract.SettableVariable("save_blob_choices"), "0")
 
-	client.SetImage("./files/1.jpg")
+	client.SetConfigFile("./config/test")
 	client.SetLanguage("por+eng")
+	client.SetImage("./files/2.jpg")
 
-	// x, y := client.GetBoundingBoxes(gosseract.PageIteratorLevel(1))
-	// fmt.Println(x, y)
 	text, err := client.Text()
 	if err != nil {
 		fmt.Printf("error %s\n", err)
