d21d29859d20c78a2aa224811ffb3792017aee83
Arthur Henrique <arthur393@gmail.com>
2020-10-25 19:24:21 -0300

    add documentation

--- main.go
+++ main.go
@@ -12,7 +12,12 @@ func main() {
 
 	client.SetConfigFile("./config/test")
 	client.SetLanguage("por+eng")
-	client.SetImage("./files/2.jpg")
+	client.SetImage("./files/3.jpg")
+
+	//http://www.sk-spell.sk.cx/tesseract-ocr-parameters-in-302-version
+
+	// Assume the input is numbers [0-9].
+	client.SetVariable("classify_bln_numeric_mode", "1")
 
 	text, err := client.Text()
 	if err != nil {
@@ -20,5 +25,12 @@ func main() {
 		return
 	}
 
+	// tesseract version
+	fmt.Println(client.Version())
+
+	// all bounding boxes
+	fmt.Println(client.GetBoundingBoxesVerbose())
+
+	// output
 	fmt.Println(text)
 }
