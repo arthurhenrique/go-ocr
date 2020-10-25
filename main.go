483c0fc8c12bc9b46665ebc85c0d3ffb849ffe03
Arthur Henrique <arthur393@gmail.com>
2020-10-25 19:28:58 -0300

    add comments

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ func main() {
 	client := gosseract.NewClient()
 	defer client.Close()
 
+	// configurations
 	client.SetConfigFile("./config/test")
 	client.SetLanguage("por+eng")
-	client.SetImage("./files/3.jpg")
+	client.SetImage("./files/2.jpg")
 
+	// tesseract parameters
 	//http://www.sk-spell.sk.cx/tesseract-ocr-parameters-in-302-version
-
-	// Assume the input is numbers [0-9].
 	client.SetVariable("classify_bln_numeric_mode", "1")
 
 	text, err := client.Text()
