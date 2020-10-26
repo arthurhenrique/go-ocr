cfe872275f8ba56e9b1bcaa75474e1d922913039
Arthur Henrique <arthur393@gmail.com>
2020-10-25 21:57:40 -0300

    page segmentation mode

--- main.go
+++ main.go
@@ -15,9 +15,15 @@ func main() {
 	client.SetLanguage("por+eng")
 	client.SetImage("./files/2.jpg")
 
-	// tesseract parameters
-	//http://www.sk-spell.sk.cx/tesseract-ocr-parameters-in-302-version
-	client.SetVariable("classify_bln_numeric_mode", "1")
+	// tesseract parameters:
+	// http: //www.sk-spell.sk.cx/tesseract-ocr-parameters-in-302-version
+
+	// auto page segmentation mode
+	client.SetVariable("tessedit_pageseg_mode", "3")
+	// allow only numerics
+	client.SetVariable("tessedit_char_whitelist", "0123456789")
+	// exclude fragments that do not look like whole characters from training and adaption
+	client.SetVariable("classify_character_fragments_garbage_certainty_threshold", "1")
 
 	text, err := client.Text()
 	if err != nil {
