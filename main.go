3760c73f3aa89c36777cff0a85aa711dec1252f1
Arthur Henrique <arthur393@gmail.com>
2020-11-28 11:54:40 -0300

    2020/11/28 14:53:45 Average: 1.346307405s 2020/11/28 14:53:45 loops: 50 2020/11/28 14:53:42 took 1.724855323s Estimating resolution as 156 bode 2020/11/28 14:53:43 took 1.398775487s Estimating resolution as 156 bode 2020/11/28 14:53:45 took 1.516603747s 2020/11/28 14:53:45 All Duration: 1m7.315370254s 2020/11/28 14:53:45 loops: 50 2020/11/28 14:53:45 Average: 1.346307405s

--- main.go
+++ main.go
@@ -25,7 +25,6 @@ func main() {
 			Variables: map[string]string{
 				"tessedit_pageseg_mode": "3", // auto page segmentation mode
 				"load_system_dawg":      "0", // removing dict to increase recognition
-				"load_freq_dawg":        "0",
 			},
 			Client: instance,
 		}
