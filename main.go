8b166907f4d3fab66427cc4f331f1fcbd1251acb
Arthur Henrique <arthur393@gmail.com>
2020-11-28 12:18:48 -0300

    ~50% speeder

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 		tm := TextMethod{
 			Name:     "tesseract",
-			Language: "eng",
+			Language: "por",
 			Variables: map[string]string{
 				"tessedit_pageseg_mode": "3", // auto page segmentation mode
 				"load_system_dawg":      "0", // removing dict to increase recognition
