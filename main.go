9086ed8707d0ebfefeee951444acb09baac7fbcc
Arthur Henrique <arthur393@gmail.com>
2020-11-28 12:03:29 -0300

    2020/11/28 15:03:12 Average: 592.928951ms 2020/11/28 15:03:11 took 545.128234ms Estimating resolution as 156 bode 2020/11/28 15:03:12 took 618.557522ms Estimating resolution as 156 bode 2020/11/28 15:03:12 took 627.186787ms 2020/11/28 15:03:12 All Duration: 29.646447589s 2020/11/28 15:03:12 loops: 50 2020/11/28 15:03:12 Average: 592.928951ms

--- main.go
+++ main.go
@@ -20,10 +20,14 @@ func main() {
 		instance = gosseract.NewClient()
 
 		tm := TextMethod{
-			Name:      "tesseract",
-			Language:  "por+eng",
-			Variables: map[string]string{},
-			Client:    instance,
+			Name:     "tesseract",
+			Language: "por",
+			Variables: map[string]string{
+				"tessedit_pageseg_mode": "3", // auto page segmentation mode
+				"load_system_dawg":      "0", // removing dict to increase recognition
+				"load_freq_dawg":        "0",
+			},
+			Client: instance,
 		}
 		defer instance.Close()
 
