d8e38584bfb791d917e475ee70b58621f3a76503
Arthur Henrique <arthur393@gmail.com>
2020-11-28 12:10:55 -0300

    2020/11/28 15:10:43 Average: 454.939291ms 2020/11/28 15:10:42 took 460.760728ms Estimating resolution as 156 bode 2020/11/28 15:10:43 took 416.192314ms Estimating resolution as 156 bode 2020/11/28 15:10:43 took 534.438062ms 2020/11/28 15:10:43 All Duration: 22.74696458s 2020/11/28 15:10:43 loops: 50 2020/11/28 15:10:43 Average: 454.939291ms

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 		tm := TextMethod{
 			Name:     "tesseract",
-			Language: "por+eng",
+			Language: "por",
 			Variables: map[string]string{
 				"tessedit_pageseg_mode": "3", // auto page segmentation mode
 				"load_system_dawg":      "0", // removing dict to increase recognition
